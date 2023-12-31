8673cb640d516231a406e75871d3c037ac9d378c
saurabhpathak <saurabhpathak072@gmail.com>
2023-12-31 22:57:45 +0530

    Create Map Picker and image picker

--- App.js
+++ App.js
@@ -1,62 +1,52 @@
+import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
 import AllPlaces from './screens/AllPlaces';
 import AddPlace from './screens/AddPlace';
-import IconButton from './components/UI/Buttons/IconButton';
-import { Colors } from './Constants/Color';
+import IconButton from './components/UI/IconButton';
+import { Colors } from './constants/colors';
+import Map from './screens/Map';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  const Stack = createNativeStackNavigator();
   return (
     <>
-      <StatusBar style="auto" />
+      <StatusBar style="dark" />
       <NavigationContainer>
         <Stack.Navigator
           screenOptions={{
             headerStyle: { backgroundColor: Colors.primary500 },
             headerTintColor: Colors.gray700,
-            contentStyle: {
-              backgroundColor: Colors.gray700,
-            },
+            contentStyle: { backgroundColor: Colors.gray700 },
           }}
         >
           <Stack.Screen
-            name="AllPlace"
+            name="AllPlaces"
             component={AllPlaces}
-            options={({ navigation, route }) => ({
-              headerRight: ({ tintColor }) => {
-                return (
-                  <IconButton
-                    color={tintColor}
-                    size={24}
-                    name={'add'}
-                    onPress={() => navigation.navigate('AddPlaces')}
-                  />
-                );
-              },
-              title: 'All Places',
+            options={({ navigation }) => ({
+              title: 'Your Favorite Places',
+              headerRight: ({ tintColor }) => (
+                <IconButton
+                  icon="add"
+                  size={24}
+                  color={tintColor}
+                  onPress={() => navigation.navigate('AddPlace')}
+                />
+              ),
             })}
           />
           <Stack.Screen
-            name="AddPlaces"
+            name="AddPlace"
             component={AddPlace}
             options={{
-              title: 'Add a New Place',
+              title: 'Add a new Place',
             }}
           />
+          <Stack.Screen name="Map" component={Map} />
         </Stack.Navigator>
       </NavigationContainer>
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
